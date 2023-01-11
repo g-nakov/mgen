e43bf43f3a4fece13eea315ed3c9ab3aad0b7339
Georgi Nakov <nakov.gl@gmail.com>
2023-01-11 16:43:47 +0000

    [doc] better documentation

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+A few examples:
+* [minimal.mgen](minimal.mgen) - a minimal example of `mgen`'s formal specification demonstrating how to use fields alongside their unit information, enumerated arrays and field-indexed arrays.
+* [common.mgen](common.mgen) - a "real-world" example demonstrating using the most common features. The generated output code can be found in [common.py](common.py) and [common.m](common.m). Accompanying input (textual) data for testing is stored in [common.in](common.in).
+* [heterogenous.mgen](heterogenous.mgen) - a small example showing how to use field-indexed arrays with different units for each entry.
+* [nesting.mgen](nesting.mgen) - an example of demonstrating nesting of structures and addressing nested fields.
---
+++ examples/common.py
@@ -0,0 +1,30 @@
+class Ivals():
+
+  __slots__ = ("lams", "kappas", "rs", "cps", "tflash", "nlayer")
+
+  def __init__(self):
+    self.lams = {}
+    self.kappas = {}
+    self.rs = {}
+    self.cps = {}
+
+
+ivals = Ivals()
+with open(fname) as f1:
+  src = f1.readlines()
+  readPtr = 0
+  ivals.nlayer = int(src[readPtr])
+  readPtr = readPtr + 1
+  ivals.tflash = 1e-3 * float(src[readPtr])
+  readPtr = readPtr + 1
+  for i in range(0, 3):
+      ivals.cps[i] = float(src[readPtr + i])
+      ivals.rs[i + 3] = 1e3 * float(src[readPtr + i + 3])
+  readPtr = readPtr + 6
+  ivals.kappas[0] = 1e-2 * float(src[readPtr + 0])
+  for i in range(1, 3):
+      ivals.kappas[i] = 1e-3 * float(src[readPtr + i])
+  readPtr = readPtr + 3
+  for i in range(0, ivals.nlayer):
+      ivals.lams[i] = 1e3 * float(src[readPtr + i])
+
